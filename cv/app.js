30105d55ac7d8223c702a166ed07e5223f919e0a
sebbaslorenzo <126308799+sebbaslorenzo@users.noreply.github.com>
2023-02-26 17:23:42 -0300

    Update app.js

--- cv/app.js
+++ cv/app.js
@@ -1,10 +1,8 @@
 
 const boton1 = document.querySelector("#boton1");
 
-// Seleccionar los elementos de información
 const info1 = document.querySelector(".info:nth-of-type(1)");
 
-// Agregar eventos de clic a los botones
 boton1.addEventListener("click", (hide) => {
 	info1.style.display = "block";
 });
